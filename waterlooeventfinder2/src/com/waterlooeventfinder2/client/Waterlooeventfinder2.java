6471b8b10bfcb6500cdf21d484c164c27af01ac4
lamajecl <mjclacom@uwaterloo.ca>
2011-10-21 13:47:24 -0400

    New MainPage

--- waterlooeventfinder2/src/com/waterlooeventfinder2/client/Waterlooeventfinder2.java
+++ waterlooeventfinder2/src/com/waterlooeventfinder2/client/Waterlooeventfinder2.java
@@ -1,21 +1,9 @@
 package com.waterlooeventfinder2.client;
-
-import com.waterlooeventfinder2.shared.FieldVerifier;
-import com.waterlooeventfinder2.shared.User;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
@@ -25,14 +13,14 @@ public class Waterlooeventfinder2 implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
-	 */
+	
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
 
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
+	
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
 
@@ -41,28 +29,43 @@ public class Waterlooeventfinder2 implements EntryPoint {
 	 */
 	public void onModuleLoad() {
 
+		final Button CategoryAll = new Button("All");
+		final Button CategoryFootball = new Button("Football");
+		final Button CategoryDance = new Button("Dance");
+		final Button CategoryConcert = new Button("Concert");
 		
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		final Button TimeUpcoming = new Button("Upcoming");
+		final Button TimeOneDay = new Button("One day");
+		final Button TimeOneWeek = new Button("One week");
+		final Button TimeOneMonth = new Button("One month");
 		
-		final Button newButton = new Button("Hello");
-
 		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
 
+		CategoryAll.addStyleName("categoryButton");
+		CategoryFootball.addStyleName("categoryButton");
+		CategoryDance.addStyleName("categoryButton");
+		CategoryConcert.addStyleName("categoryButton");
+
+		TimeUpcoming.addStyleName("timeButton");
+		TimeOneDay.addStyleName("timeButton");
+		TimeOneWeek.addStyleName("timeButton");
+		TimeOneMonth.addStyleName("timeButton");
+		
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("sendButtonContainer").add(newButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+				
+		RootPanel.get("categoryButton").add(CategoryAll);
+		RootPanel.get("categoryButton").add(CategoryFootball);
+		RootPanel.get("categoryButton").add(CategoryDance);
+		RootPanel.get("categoryButton").add(CategoryConcert);
 
+		RootPanel.get("timeButton").add(TimeUpcoming);
+		RootPanel.get("timeButton").add(TimeOneDay);
+		RootPanel.get("timeButton").add(TimeOneWeek);
+		RootPanel.get("timeButton").add(TimeOneMonth);
+		
 		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
+		
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
 		dialogBox.setText("Remote Procedure Call");
@@ -70,39 +73,28 @@ public class Waterlooeventfinder2 implements EntryPoint {
 		final Button closeButton = new Button("Close");
 		// We can set the id of a widget by accessing its Element
 		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
+		
+		// TO ADD A CSS STYLE dialogVPanel.addStyleName("dialogVPanel");
+		
+		CategoryAll.setEnabled(true);
+		CategoryAll.setFocus(true);
+		CategoryFootball.setEnabled(true);
+		CategoryDance.setEnabled(true);
+		CategoryConcert.setEnabled(true);
+		
+		
 		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
+		/*class MyHandler implements ClickHandler, KeyUpHandler {
 			/**
 			 * Fired when the user clicks on the sendButton.
-			 */
+			 
 			public void onClick(ClickEvent event) {
 				sendNameToServer();
 			}
 
 			/**
 			 * Fired when the user types in the nameField.
-			 */
+			 
 			public void onKeyUp(KeyUpEvent event) {
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
 					sendNameToServer();
@@ -111,7 +103,7 @@ public class Waterlooeventfinder2 implements EntryPoint {
 
 			/**
 			 * Send the name from the nameField to the server and wait for a response.
-			 */
+			 
 			private void sendNameToServer() {
 				// First, we validate the input.
 				errorLabel.setText("");
@@ -148,11 +140,13 @@ public class Waterlooeventfinder2 implements EntryPoint {
 							}
 						});
 			}
-		}
+		}*/
 
 		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
+		/*MyHandler handler = new MyHandler();
+		
 		sendButton.addClickHandler(handler);
 		nameField.addKeyUpHandler(handler);
+		*/
 	}
 }
