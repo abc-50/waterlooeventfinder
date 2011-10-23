8768cdefc7f0161601ee00ca72e61bb9ad536bef
abc-50 <0disappointed0@gmail.com>
2011-10-23 13:44:34 -0400

    Repo clean up. deleted projects waterlooeventfinder and waterlooeventfinderCS446

--- waterlooeventfinder/src/com/google/gwt/project/waterlooeventfinder/client/RetrieveEventsService.java
+++
@@ -1,8 +0,0 @@
-package com.google.gwt.project.waterlooeventfinder.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.RemoteService;
-
-public interface RetrieveEventsService extends RemoteService {
-	String getEvents(String input) throws IllegalArgumentException;
-}
